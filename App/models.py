fef71983d43a7940cb16debbd8cd48d9069cbdac
munesha55 <78863051+munesha55@users.noreply.github.com>
2022-04-26 18:37:41 +0000

    finished models

--- App/models.py
+++ App/models.py
@@ -12,3 +12,26 @@ def create_db(app):
     
 def init_db(app):
     db.init_app(app)
+
+class Review(db.Model):
+	id = db.Column('id', db.Integer, primary_key=True)
+	text = db.Column('text', db.String(200), nullable=False)
+	rating = db.Column('rating', db.Integer, nullable=False)
+	isbn = db.Column('isbn', db.ForeignKey('book.isbn'))
+
+class Book(db.Model):
+	isbn = db.Column('isbn', db.String(100), primary_key=True, nullable=False)
+	title = db.Column('title', db.String(100), nullable=False)
+	author = db.Column('author', db.String(50), nullable=False)
+	publication_year = db.Column('publication_year', db.Integer, nullable=False)
+	publisher = db.Column('publisher', db.String(50), nullable=False)
+	reviews = db.relationship('Review', backref='book', cascade='all, delete-orphan', lazy=True)
+	url_s = db.Column('url_s', db.String(100), nullable=True)
+	url_m = db.Column('url_m', db.String(100), nullable=True)
+	url_l = db.Column('url_l', db.String(100), nullable=True)
+
+	def get_avg_rating(self):
+		sum = 0
+		for review in self.reviews:
+			sum += review.rating
+		return sum / len(self.reviews)
