fa1d79789e93278cc9fe513a5354a74eb8c215ff
munesha55 <78863051+munesha55@users.noreply.github.com>
2022-04-26 19:42:46 +0000

    Changed Books.csv and successfully created db

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
-import click
-from flask import Flask
+import csv
 from App import create_db, db, app
+from App.models import Book
 
 
 @app.cli.command("init")
 def initialize():
-    create_db(app)
-    print('database intialized')
+	create_db(app)
+	file = open('books.csv', 'r')
+	rows = csv.DictReader(file)
+	for row in rows:
+		newBook = Book()
+		newBook.isbn = row['ISBN']
+		newBook.title = row['Book-Title']
+		newBook.author = row['Book-Author']
+		newBook.publication_year = row['Year-Of-Publication']
+		newBook.publisher = row['Publisher']
+		newBook.url_s = row['Image-URL-S']
+		newBook.url_m = row['Image-URL-M']
+		newBook.url_l = row['Image-URL-L']
+		db.session.add(newBook)
+		db.session.commit()
+	print('database intialized')
