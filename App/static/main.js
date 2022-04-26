53406ed8876b0b30b735891f92981c8ae982ed0e
munesha55 <78863051+munesha55@users.noreply.github.com>
2022-04-26 22:13:31 +0000

    get form to work

--- App/static/main.js
+++ App/static/main.js
@@ -32,3 +32,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
 	// books.innerHTML = `<h1>TEST</h1>`;
 	load_books();
 });
+
+async function get_reviews(formData){
+	let response = await fetch(`/createReview/${formData}`);
+	let results = await response.json();
+	return results;
+}
+
+const reviews = document.getElementById("reviewList");
+function addReview(e){	
+	let formData = new FormData(document.getElementById("reviewForm"));
+	// let results = get_reviews(formData);
+	let html = `<h1>${formData.get('text')}</h1>`;
+	// for (let review of results){
+		
+	// }
+	reviews.innerHTML = html;
+	e.preventDefault();
+}
